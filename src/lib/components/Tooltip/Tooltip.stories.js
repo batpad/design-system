1377fabf99d9f1d2a33abd9f4ff6fd8c2014d7d2
efarooqui <efarooqui@pandora.com>
2020-09-21 09:38:58 -0700

    Finished styling tooltip with caret at end of card; modified stories accordingly

--- src/lib/components/Tooltip/Tooltip.stories.js
+++ src/lib/components/Tooltip/Tooltip.stories.js
@@ -11,16 +11,20 @@ const Template = (args) => <Tooltip {...args} />;
 
 const TooltipText = ({ text }) => {
   return (
-    <div
-      className="content"
-    >
-      <h1>Title</h1>
-      <p>{text}</p>
+    <div>
+        <div className="tooltip-tip">
+        </div>
+      <div
+        className="content"
+      >
+        <h1>Title</h1>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
 
 export const TooltipWithText = Template.bind({});
 TooltipWithText.args = {
-  children: <TooltipText text="title content!"/>
+  children: <TooltipText text="lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! lots of text! "/>
 };
